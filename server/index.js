33a0a5a5337a5f764fc23542728efc4020c9bd23
suman7490 <suman.dhanagar.135497@gmail.com>
2025-08-17 02:34:50 -0700

    added

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import tourRoute from './routes/tours.js';
-import userRoute from './routes/users.js';
-import authRoute from './routes/auth.js';
-import reviewRoute from './routes/reviews.js';
-import bookingRoute from './routes/bookings.js';
+import express from 'express'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import tourRoute from './routes/tours.js'
+import userRoute from './routes/users.js'
+import authRoute from './routes/auth.js'
+import reviewRoute from './routes/reviews.js'
+import bookingRoute from './routes/bookings.js'
 import path from 'path';
 import { fileURLToPath } from 'url';
 import serverless from 'serverless-http';
@@ -15,27 +15,43 @@ import serverless from 'serverless-http';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-dotenv.config();
-const app = express();
+dotenv.config()
+const app = express()
 
-// middlewares
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
-app.use(express.json());
-app.use(cookieParser());
+const port = process.env.PORT || 8000;
+const corsOptions = {
+    // origin: 'http://localhost:3000',
+    origin: process.env.CLIENT_URL || "*",
+    credentials: true,
+}
+app.use(cors(corsOptions))
+app.use(express.json())
+app.use(cookieParser())
 
-// routes
-app.use('/api/v1/auth', authRoute);
-app.use('/api/v1/tours', tourRoute);
-app.use('/api/v1/users', userRoute);
-app.use('/api/v1/review', reviewRoute);
-app.use('/api/v1/booking', bookingRoute);
+app.use('/api/v1/auth', authRoute)
+app.use('/api/v1/tours', tourRoute)
+app.use('/api/v1/users', userRoute)
+app.use('/api/v1/review', reviewRoute)
+app.use('/api/v1/booking', bookingRoute)
 app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
 
-// db connect
-mongoose.set('strictQuery', false);
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log("MongoDB connected"))
-    .catch(err => console.log("DB error:", err));
 
-// export for vercel
-export const handler = serverless(app);
+
+
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('Database connected')
+    } catch (err) {
+        console.log('Database connection failed')
+    }
+}
+
+app.listen(port, () => {
+    connect();
+    console.log('Server listening o port', port)
+})
+
