75d7d14aef3bde000949064c415c4240bca0303e
Suman7490 <suman.dhanagar.135497@gmail.com>
2025-09-18 21:02:01 +0530

    added correct origin

--- server/index.js
+++ server/index.js
@@ -24,7 +24,8 @@ const app = express();
 const allowedOrigins = [
     "http://localhost:3000",
     "https://go4holidays.vercel.app",
-    "https://www.makemyatra.com/"
+    "https://makemyatra.com",
+    "https://www.makemyatra.com"
 ];
 app.use(express.json());
 app.use(cookieParser());
